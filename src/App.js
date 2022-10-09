ecf43d846cdcd40a28c04822998635b7dea61300
IncredibleZuess <carlo.barnardo@gmail.com>
2022-10-09 21:33:52 +0200

    Implemented localStorage feature

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ import Button from 'react-bootstrap/Button';
 import DataSource from './datasource';
 
 function App() {
+  const localStorage = window.localStorage
   const allCards = DataSource.getAllFlashcards();
   const totalCards = allCards.length;
   const [curCardId, setCurCardId] = useState(1);
+
+  //Store all files from the data source in local storage otherwise none of the cards can be rendered :)
+  allCards.map((card)=>{
+    return localStorage.setItem(card.id,JSON.stringify({id: card.id ,front: card.front, back:card.back}) )
+  })
+
+  //Convert the cards back from local storage to an array 
+  const getAllCards = () =>{
+    let cards = []
+    for(let i = 1; i<=localStorage.length; i++){
+      let card = JSON.parse(localStorage.getItem(i))
+      cards.push(card)
+    }
+    return cards
+  }
+  let newCards = getAllCards()
 
   let goToPrev = () => {
     if ( isValidId(curCardId - 1)) {
@@ -36,7 +53,7 @@ function App() {
   return (
     <Container className="container-main">
       <Toolbar></Toolbar>
-      <Card {...allCards.find(item=> item.id === curCardId)}></Card>
+      <Card {...newCards.find(item=> item.id === curCardId)}></Card>
       <div className="action-buttons">
         <Button variant="warning" className="me-4 prev-next-btns" onClick={goToPrev}>Prev</Button>
         <Button variant="success" className="ms-4 prev-next-btns" onClick={goToNext}>Next</Button>
