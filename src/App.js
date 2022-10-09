570c47dfe35e028be28b0eda745d998275e74669
Prerit Kumar Jha <preritkrjha@gmail.com>
2022-10-10 00:45:10 +0530

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './components/Card';
 import Toolbar from './components/Toolbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import DataSource from './datasource';
+import {BsFillMoonFill, BsFillSunFill} from 'react-icons/bs'
+
+function getWindowDimensions() {
+  const { innerHeight: height } = window;
+  return {
+    height
+  };
+}
 
 function App() {
   const allCards = DataSource.getAllFlashcards();
   const totalCards = allCards.length;
   const [curCardId, setCurCardId] = useState(1);
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [mode, setMode] = useState('light');
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   let goToPrev = () => {
     if ( isValidId(curCardId - 1)) {
@@ -34,6 +53,21 @@ function App() {
   }
 
   return (
+    <div style={{  
+      backgroundColor: mode === 'light' ? "rgb(204, 248, 204)" : "rgb(0, 0, 0)",
+      backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: windowDimensions.height,
+    }}>
+    <div style={{float: 'right', marginRight: "30px", marginTop: "20px"}}>
+    <input type="checkbox" class="checkbox" id="checkbox" />
+      <label for="checkbox" class="label" onClick={e => setMode(mode === 'light' ? 'dark' : 'light')}>
+        <BsFillMoonFill style={{color: "pink"}}/>
+        <BsFillSunFill style={{color: "yellow"}}/>
+        <div class='ball'></div>
+      </label>
+    </div>
     <Container className="container-main">
       <Toolbar></Toolbar>
       <Card {...allCards.find(item=> item.id === curCardId)}></Card>
@@ -42,6 +76,7 @@ function App() {
         <Button variant="success" className="ms-4 prev-next-btns" onClick={goToNext}>Next</Button>
       </div>
     </Container>
+    </div>
   );
 }
 
