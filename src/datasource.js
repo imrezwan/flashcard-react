cabca9db2a6d9fe20a1e06306ed493433c03175f
M Urwah Shahid <107793187+urwah1248@users.noreply.github.com>
2022-10-09 21:38:37 +0500

    Added one card for "State"
    Just added one more dataset for another card in the app.

--- src/datasource.js
+++ src/datasource.js
@@ -3,6 +3,7 @@ const flashcards = [
     { id: 2, front: "Props", back: "When React sees an element representing a user-defined component, it passes JSX attributes and children to this component as a single object. We call this object “props”." },
     { id: 3, front: "Conditional Rendering", back: "Conditional rendering in React works the same way conditions work in JavaScript. Use JavaScript operators like if or the conditional operator to create elements representing the current state, and let React update the UI to match them." },
     { id: 4, front: "Composition vs Inheritance", back: "React has a powerful composition model, and we recommend using composition instead of inheritance to reuse code between components." },
+    { id: 5, front: "State", back: "State is a built-in object in React Library. It is used to contain information or data about its containing component. State re-renders the component upon a change in it's state."},
 ];
 
 export default class DataSource {
