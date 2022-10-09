8408ada17c18797607a88ea954e2ee375aae1133
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-10-09 13:54:28 +0530

    removed package-lock.json

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-export default function Card({id, front, back}) {
-
-    return (
-       <div className="card-holder">
-            <div className="card">
-                <div className="front">
-                    <p>{id}</p>
-                    <p>{front}</p>
-                </div>
-                <div className="back">
-                    <p>{back}</p>
-                </div>
-            </div>
-       </div>
-    )
+export default function Card({ id, front, back }) {
+  return (
+    <div className='card-holder'>
+      <div className='card'>
+        <div className='front'>
+          <p>{id}</p>
+          <p>{front}</p>
+        </div>
+        <div className='back'>
+          <p>{back}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
