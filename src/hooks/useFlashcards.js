a969cd5f6d876d413e173d91cee47a07a4015521
tchar7 <115107146+tchar7@users.noreply.github.com>
2022-10-10 20:20:15 +0530

    Added Flashcard content

--- src/hooks/useFlashcards.js
+++ src/hooks/useFlashcards.js
@@ -5,6 +5,8 @@ const INITIAL_FLASHCARDS = [
     { id: 2, front: "Props", back: "When React sees an element representing a user-defined component, it passes JSX attributes and children to this component as a single object. We call this object “props”." },
     { id: 3, front: "Conditional Rendering", back: "Conditional rendering in React works the same way conditions work in JavaScript. Use JavaScript operators like if or the conditional operator to create elements representing the current state, and let React update the UI to match them." },
     { id: 4, front: "Composition vs Inheritance", back: "React has a powerful composition model, and we recommend using composition instead of inheritance to reuse code between components." },
+    { id: 5, front: "Uses", back: " It's used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript." },
+    { id: 6, front: "Type", back: "ReactJS is an open-source, component-based front-end library responsible only for the view layer of the application." },
 ];
 
 const FLASHCARDS_KEY = 'flashcards';
