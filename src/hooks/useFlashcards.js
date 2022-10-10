acd06f7aeb2ad79bf5fd2b8c526e48a896fca6cf
Sushan Shakya <sushaanshakya88@gmail.com>
2022-10-10 11:15:26 +0545

    Feat: Add a new Data item

--- src/hooks/useFlashcards.js
+++ src/hooks/useFlashcards.js
@@ -5,6 +5,7 @@ const INITIAL_FLASHCARDS = [
     { id: 2, front: "Props", back: "When React sees an element representing a user-defined component, it passes JSX attributes and children to this component as a single object. We call this object “props”." },
     { id: 3, front: "Conditional Rendering", back: "Conditional rendering in React works the same way conditions work in JavaScript. Use JavaScript operators like if or the conditional operator to create elements representing the current state, and let React update the UI to match them." },
     { id: 4, front: "Composition vs Inheritance", back: "React has a powerful composition model, and we recommend using composition instead of inheritance to reuse code between components." },
+    { id: 5, front: "State", back: "React State can be thought of as a variable in which we store some data. We can tell React to re-render our view when these \"state\" variables change by using setState()." },
 ];
 
 const FLASHCARDS_KEY = 'flashcards';
